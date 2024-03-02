29985b7081f1a9b4d3bf2f34d59dd2679d48a406
karenhol7 <karenhol137@gmail.com>
2024-03-01 19:56:59 -0500

    ADD

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -5,26 +5,38 @@ let tasks = [
 
     {
         id: 1,
-        body: ''
+        title: 'Nuevo cliente',
+        body: 'Generar la cotización del nuevo cliente',
+        manager: 'Karen Holguín',
+        notes: 'Costos 2023',
+        state: 'Finalizado'
     },
     {
         id: 2,
-        body: ''
+        title: 'Certificado',
+        body: 'Generar y enviar el nuevo certificado',
+        manager: 'Ponchita Dolores',
+        notes: '',
+        state: 'En proceso'
     },
     {
         id: 3,
-        body: ''
+        title: 'Homologación',
+        body: 'Homologar los estudios y analitos',
+        manager: 'Pancracio Desgracias',
+        notes: 'Homologar por CUPS',
+        state: 'Pendiente'
     }
 ]
 
 
-//Get all tasks
+//Function to get all tasks
 const getAllTasks = (req, res = response) => {
     res.statusCode = 200;
     res.json(tasks);
 };
 
-//Get task by id
+//Function to get task by its id
 const getTaskById = (req, res = response) => {
     const taskId = parseInt(req.params.id);
     const task = tasks.find(t => t.id === taskId);
@@ -32,27 +44,121 @@ const getTaskById = (req, res = response) => {
     if (task) {
         res.status(200).json(task);
     } else {
-        res.status(404).json({ error: 'Not Found' });
+        res.status(404).json({
+            error: {
+                code: "404",
+                message: "No encontrada"
+            }
+        });
     }
 }
 
-//Create a new task 
+//Function to create a new task 
 const createNewTask = (req, res = response) => {
-
+    const { title } = req.body;
     const { body } = req.body;
+    const { manager } = req.body;
+    const { notes } = req.body;
+    const { state } = req.body;
+
+    if (!title) {
+        return res.status(400).json({
+            error: {
+                code: "400",
+                message: "El titulo de la tarea es requerido"
+            }
+        });
+    }
+    if (!manager) {
+        return res.status(400).json({
+            error: {
+                code: "400",
+                message: "El responsable de la tarea es requerido"
+            }
+        });
+    }
+
     const newTask = {
         id: tasks.length + 1,
-        body
+        title,
+        body,
+        manager,
+        notes,
+        state
     };
 
     tasks.push(newTask);
 
-    res.status(201).json("Created");
+    res.status(201).json({
+        success: true,
+        message: "La tarea ha sido creada correctamente",
+        code: "201"
+    });
 }
+
+//Function to update a task by its id
+const updateTask = (req, res = response) => {
+    const taskId = parseInt(req.params.id);
+
+    const { title } = req.body;
+    const { body } = req.body;
+    const { manager } = req.body;
+    const { notes } = req.body;
+    const { state } = req.body;
+
+    const existingTask = tasks.find(task => task.id === taskId);
+
+    if (!title) {
+        return res.status(400).json({
+            error: {
+                code: "400",
+                message: "El titulo de la tarea es requerido"
+            }
+        });
+    }
+    if (!manager) {
+        return res.status(400).json({
+            error: {
+                code: "400",
+                message: "El responsable de la tarea es requerido"
+            }
+        });
+    }
+
+    existingTask.title = title;
+    existingTask.body = body;
+    existingTask.manager = manager;
+    existingTask.notes = notes;
+    existingTask.state = state;
+
+
+    res.status(200).json({
+        success: true,
+        message: "La tarea ha sido actualizada correctamente",
+        code: "200"
+    });
+};
+
+//Function to delete a task by its id
+const deleteTask = (req, res = response) => {
+    const taskId = parseInt(req.params.id);
+
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    const deletedTask = tasks.splice(taskIndex, 1)[0];
+
+    res.status(201).json({
+        success: true,
+        message: "La tarea ha sido eliminada correctamente",
+        code: "204"
+    });
+};
 
 //Export variables
 module.exports = {
     getAllTasks,
     getTaskById,
-    createNewTask
+    createNewTask,
+    updateTask,
+    deleteTask
 } 
--- routers/tasksRouter.js
+++ routers/tasksRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getAllTasks, getTaskById, createNewTask } = require('../controllers/tasksController');
+const { getAllTasks, getTaskById, createNewTask, updateTask, deleteTask } = require('../controllers/tasksController');
 
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.get("/tasks", getAllTasks);
 router.get("/tasks/:id", getTaskById);
 router.post("/tasks", createNewTask);
+router.put("/tasks/:id", updateTask);
+router.delete("/tasks/:id", deleteTask)
 
 module.exports = router
